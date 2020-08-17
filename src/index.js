42c3e1c73e98ff935e4c997a648d296e65f896f8
Dhinesh Kumar <dhins333@gmail.com>
2020-08-17 20:56:59 +0530

    shuffle words

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const api = require('./routes/api');
 
 const app = express();
 
 app.use(express.json());
+app.use(api);
 
 app.get('*',(req,res) => {
     res.status(200).send('Test');
