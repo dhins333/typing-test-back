f25c6291b8a019c1ab7b89f8bd959161f3da071e
Dhinesh Kumar <dhins333@gmail.com>
2020-08-18 14:28:24 +0530

    build 1

---
+++ public/precache-manifest.48e408a3926d332856db27c502789cf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d481228160ed07d7b9081ed9548b8628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0886dcd540a7bbde74c",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2b6b027e5c58ba47fd60",
+    "url": "/static/css/main.27e500b8.chunk.css"
+  },
+  {
+    "revision": "d0886dcd540a7bbde74c",
+    "url": "/static/js/2.a8dc9e75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8dc9e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6b027e5c58ba47fd60",
+    "url": "/static/js/main.5b195a41.chunk.js"
+  },
+  {
+    "revision": "718ce3678678957d944d",
+    "url": "/static/js/runtime-main.03b02dac.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 const api = require('./routes/api');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
 app.use(api);
 
 app.get('*',(req,res) => {
-    res.status(200).send('Test');
+    res.sendFile(path.join(__dirname,'..','public','index.html'));
 })
 
 app.listen(process.env.PORT,(req,res) => {
