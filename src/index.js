a69f77a47dc6f9d774ac57f68e3b33db7950d7d6
Dhinesh Kumar <dhins333@gmail.com>
2020-09-10 16:13:34 +0530

    word-box

--- src/index.js
+++ src/index.js
@@ -4,7 +4,70 @@ const socketio = require('socket.io');
 const http = require('http');
 const api = require('./routes/api');
 const Queue = require('./queue');
+const shuffle = require('shuffle-array');
 const socketMap = {};
+const words = [
+'lift',
+'sin',
+'break',
+'ban',
+'cool',
+'evoke',
+'blank',
+'quota',
+'utter',
+'crop',
+'elect',
+'scan',
+'still',
+'strap',
+'beach',
+'jest',
+'share',
+'meal',
+'ally',
+'cheek',
+'loose',
+'doubt',
+'stage',
+'spite',
+'coast',
+'colon',
+'hour',
+'flash',
+'in',
+'ice',
+'pull',
+'pause',
+'onion',
+'jail',
+'fan',
+'mean',
+'limit',
+'floor',
+'tick',
+'issue',
+'time',
+'coma',
+'liver',
+'beg',
+'weed',
+'means',
+'abuse',
+'upset',
+'snub',
+'quota',
+'salad',
+'agent',
+'model',
+'feel',
+'tiger',
+'hero',
+'soap',
+'abbey',
+'hate',
+'pill',
+'steam'];
 
 const app = express();
 const server = http.createServer(app);
@@ -38,7 +101,7 @@ io.on('connect',(socket) => {
         if(socketMap[socket.id]){
             delete socketMap[socket.id];
         }
-        else{
+        else if(queue.len != 0){
             queue.remove(socket.id);
         }
         console.log(socketMap);
@@ -59,8 +122,9 @@ const pairing = () => {
             const b = queue.dequeue().data;
             socketMap[a] = b;
             socketMap[b] = a;
-            io.to(a).emit('matched',socketMap[a]);
-            io.to(b).emit('matched',socketMap[b]);
+            const temp = shuffle(words);
+            io.to(a).emit('matched',temp);
+            io.to(b).emit('matched',temp);
         }
     },5000)
 }
