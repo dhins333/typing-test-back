b4dff5c7c673422f7144af05479f510cf5f64d88
Dhinesh Kumar <dhins333@gmail.com>
2020-09-23 19:24:23 +0530

    code cleaned

--- src/index.js
+++ src/index.js
@@ -2,73 +2,13 @@ const express = require('express');
 const path = require('path');
 const socketio = require('socket.io');
 const http = require('http');
+const shuffle = require('shuffle-array');
+
 const api = require('./routes/api');
 const Queue = require('./queue');
-const shuffle = require('shuffle-array');
-const socketMap = {};
-const words = [
-'lift',
-'sin',
-'break',
-'ban',
-'cool',
-'evoke',
-'blank',
-'quota',
-'utter',
-'crop',
-'elect',
-'scan',
-'still',
-'strap',
-'beach',
-'jest',
-'share',
-'meal',
-'ally',
-'cheek',
-'loose',
-'doubt',
-'stage',
-'spite',
-'coast',
-'colon',
-'hour',
-'flash',
-'in',
-'ice',
-'pull',
-'pause',
-'onion',
-'jail',
-'fan',
-'mean',
-'limit',
-'floor',
-'tick',
-'issue',
-'time',
-'coma',
-'liver',
-'beg',
-'weed',
-'means',
-'abuse',
-'upset',
-'snub',
-'quota',
-'salad',
-'agent',
-'model',
-'feel',
-'tiger',
-'hero',
-'soap',
-'abbey',
-'hate',
-'pill',
-'steam'];
+const words = require('./words');
 
+const socketMap = {};
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -84,38 +24,34 @@ app.get('*',(req,res) => {
 
 io.on('connect',(socket) => {
 
-    // console.log(socket.id);
-    
-    // socketMap[socket.id] = undefined;
-    // console.log(queue,socketMap);
-
     socket.on('queue',() => {
         queue.enqueue(socket.id);    
     })
 
     socket.on('disconnect',() => {
-        // delete socketMap[socket.id];
-        // console.log(socketMap);
-        // console.log('disconnected');
-        io.to(socketMap[socket.id]).emit('op_disconnect','Opponent Disconnected');
         if(socketMap[socket.id]){
+            if(socketMap[socket.id] !== 'done'){
+                io.to(socketMap[socket.id]).emit('op_disconnect','Opponent Disconnected');
+            }
             delete socketMap[socket.id];
         }
         else if(queue.len != 0){
             queue.remove(socket.id);
         }
-        console.log(socketMap);
     })
 
+    socket.on('progress',() => {
+        io.to(socketMap[socket.id]).emit('op_progress');
+    })
 
+    socket.on('done',() => {
+        io.to(socketMap[socket.id]).emit('lose');
+        socketMap[socket.id] = 'done';
+    })
 
 } )
 
 server.listen(process.env.PORT,(req,res) => {
-    console.log('Server Started');
-});
-
-const pairing = () => {
     setInterval(() => {
         if(queue.len >= 2){
             const a = queue.dequeue().data;
@@ -127,6 +63,4 @@ const pairing = () => {
             io.to(b).emit('matched',temp);
         }
     },5000)
-}
-
-pairing();
+});
